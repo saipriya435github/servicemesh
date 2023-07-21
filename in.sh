cb8e12ec4d4f063755ece9e9f5aa8e37352176d6
saipriya435github <107021418+saipriya435github@users.noreply.github.com>
2023-07-21 13:32:08 +0530

    Created in.sh

---
+++ in.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update the package list and install required packages
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+# Add the Docker repository key and repository
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Add the Kubernetes repository key and repository
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Update the package list again to get the new repositories
+sudo apt-get update
+
+# Install Docker and Kubernetes
+sudo apt-get install -y docker-ce kubelet=1.15.7-00 kubeadm=1.15.7-00 kubectl=1.15.7-00
